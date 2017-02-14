6e1570038711e57672deb20f3e428d9453818af0
Sávio Carlos Martins Costa <saviocmc@gmail.com>
2017-02-14 01:46:32 -0200

    Using the getTime() method inside the getDistance() method.

--- Ultrasonic.cpp
+++ Ultrasonic.cpp
@@ -9,16 +9,6 @@ Ultrasonic::Ultrasonic(byte trigPin, byte echoPin)
    this->echoPin = echoPin;
 }
 
-long Ultrasonic::getDistance()
-{
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  return pulseIn(echoPin,HIGH)/5.8L;
-}
-
 long Ultrasonic::getTime()
 {
   digitalWrite(trigPin, LOW);
@@ -27,4 +17,9 @@ long Ultrasonic::getTime()
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   return pulseIn(echoPin,HIGH);
+}
+
+long Ultrasonic::getDistance()
+{
+  return this->getTime()/5.8L;
 }
